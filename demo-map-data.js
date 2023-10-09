b39f15241150186a815b0c99158726eebb380f6f
thomasm1248 <thomasm1248@gmail.com>
2023-10-09 17:01:22 -0600

    Make map viewer

---
+++ demo-map-data.js
@@ -0,0 +1,36 @@
+data = {
+    map: {
+        width: 10,
+        height: 10
+    },
+    log: [
+        {
+            tank: "Big Fat Tank",
+            action: "Moved to (0,4)"
+        },
+        {
+            tank: "Another One",
+            action: "Shot Big Fat Tank"
+        }
+    ],
+    tanks: [
+        {
+            name: "Big Fat Tank",
+            bio: "I'm a tank.",
+            lives: 2,
+            action_points: 2,
+            range: 2,
+            x: 3,
+            y: 8
+        },
+        {
+            name: "Another One",
+            bio: "I'm another tank.",
+            lives: 3,
+            action_points: 2,
+            range: 2,
+            x: 6,
+            y: 2
+        }
+    ]
+};
