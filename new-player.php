59a9c73581df371aab78f4cf2ea2afd3fa127efe
thomasm1248 <thomasm1248@gmail.com>
2023-10-25 10:07:23 -0600

    Get new player tank creation working

---
+++ new-player.php
@@ -0,0 +1,74 @@
+<?php
+
+function spotTaken($x, $y, $xs, $ys) {
+    for($i = 0; $i < sizeof($xs); $i+=1) {
+        if($x === $xs[$i] && $y === $ys[$i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Connect to database
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "Game";
+$conn = mysqli_connect($host, $username, $password, $database);
+// Check if connection was successful
+if (!$conn) {
+    print("fail");
+}
+
+// Get new player information
+$sessionid = $_POST['sessionid'];
+$pagecode = $_POST['pagecode'];
+$playername = $_POST['player-name'];
+$bio = addslashes($_POST['bio']);
+$playercode = rand();
+
+// Get height and width of table
+$sql = "SELECT `width`, `height` FROM `Sessions` WHERE `sessionid` = $sessionid;";
+$row = mysqli_fetch_array(mysqli_query($conn, $sql));
+$width = $row['width'];
+$height = $row['height'];
+
+// Get locations of other tanks
+$sql = "SELECT `x`, `y` FROM `Tanks` WHERE `partofsession` = $sessionid;";
+$result = mysqli_query($conn, $sql);
+$xs = array();
+$ys = array();
+print(mysqli_fetch_array($result));
+while($row = mysqli_fetch_array($result)) {
+    $xs[] = $row['x'];
+    $ys[] = $row['y'];
+}
+
+// Determine where to place new player on the map
+$x = rand(0, $width - 1);
+$y = rand(0, $height - 1);
+$i=0;
+while(spotTaken($x, $y, $xs, $ys)) {
+    print("($x,$y)");
+    $x = rand(0, $width - 1);
+    $y = rand(0, $height - 1);
+    $i+=1;
+    if($i > 20) {
+        print("timeout");
+        break;
+    }
+}
+
+print("foo");
+// Add new player to database
+$sql = "INSERT INTO `Tanks`(`name`, `pagecode`, `partofsession`, `bio`, `x`, `y`) VALUES ('$playername', $playercode, $sessionid, '$bio', $x, $y);";
+print($sql);
+$result = mysqli_query($conn, $sql);
+mysqli_close($conn);
+print("baz");
+
+// Redirect to join success page
+include('join-success.php');
+header("Location: $url/join-success.php?session=$pagecode");
+
+?>
--- session-home.php
+++ session-home.php
@@ -21,11 +21,13 @@
         }
 
         // Get table row for session
-        $sql = "SELECT `name`, `status` FROM `Sessions` WHERE `pagecode` = $pagecode;";
+        $sql = "SELECT `name`, `sessionid`, `status` FROM `Sessions` WHERE `pagecode` = $pagecode;";
         $result = mysqli_query($conn, $sql);
         if(mysqli_num_rows($result)) {
             $row = mysqli_fetch_array($result);
             $sessionname = $row["name"];
+            $sessionid = $row["sessionid"];
+            $status = $row["status"];
         } else {
             $sessionname = "Error: Invalid URL";
         }
@@ -53,18 +55,29 @@
             
             <h2><?php print($sessionname); ?></h2>
 
-            <h3>Player Login</h3>
-            <form action="POST">
-                
-                <input id="player-name" name="player-name" type="text" placeholder="player name"><br>
+            <!-- Show link to map if session is running -->
+            <?php if($status === "running") { ?>
+                <a href="view-map.php?session=<?php print($pagecode); ?>"><h3>View Map</h3></a>
+            <?php } ?>
 
-                <input id="password" name="password" type="password" placeholder="password"><br>
+            <!-- Let players join session if session is waiting -->
+            <?php if($status === "waiting") { ?>
+                <p>This session hasn't started yet. Players are free to join until it starts.</p>
+                <h3>Join This Session</h3>
+                <form action="new-player.php" method="post">
+                    
+                    <input id="sessionid" name="sessionid" type="hidden" value="<?php print($sessionid); ?>">
 
-                <input id="login" type="submit" value="Login">
+                    <input id="pagecode" name="pagecode" type="hidden" value="<?php print($pagecode); ?>">
 
-            </form>
+                    <input id="player-name" name="player-name" type="text" placeholder="player name"><br>
 
-            <a href="view-map.php?session=<?php print($pagecode); ?>"><h3>View Map</h3></a>
+                    <textarea id="bio" name="bio" placeholder="player bio and/or contact info" cols="30" rows="15"></textarea><br>
+
+                    <input id="join" type="submit" value="Join">
+
+                </form>
+            <?php } ?>
 
             <h3>Share This Session</h3>
             <p>Share this page with anyone who wants to watch this session.</p>
